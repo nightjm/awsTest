41f0a1ff9aa74b796fd061e4b6da4ee87ad74ce4
cobien <cobien@DESKTOP-OJO925U>
2023-08-02 13:19:44 +0900

    testppp

--- src/main/java/com/example/ewstest/HelloServlet.java
+++ src/main/java/com/example/ewstest/HelloServlet.java
@@ -23,7 +23,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
-        out.println("</body></html> ");
+        out.println("</body></html>   ");
     }
 
     public void destroy() {
