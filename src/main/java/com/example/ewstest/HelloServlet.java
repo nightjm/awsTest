cba9ae34877498249ab4cc88e452471ff51df285
cobien <cobien@DESKTOP-OJO925U>
2023-08-02 13:12:57 +0900

    Test

--- src/main/java/com/example/ewstest/HelloServlet.java
+++ src/main/java/com/example/ewstest/HelloServlet.java
@@ -1,8 +1,12 @@
 package com.example.ewstest;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "helloServlet", value = "/test")
 public class HelloServlet extends HttpServlet {
@@ -19,7 +23,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        out.println("</body></html> ");
     }
 
     public void destroy() {
