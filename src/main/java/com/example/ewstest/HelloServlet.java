a832657c75ca421fe3802c519ebe363cc2dcd347
cobien <cobien@DESKTOP-OJO925U>
2023-09-01 17:02:01 +0900

    test11

--- src/main/java/com/example/ewstest/HelloServlet.java
+++ src/main/java/com/example/ewstest/HelloServlet.java
@@ -23,7 +23,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>!!고고고임대리");
+        out.println("</body></html>!!고고고임대리11");
     }
 
     public void destroy() {
